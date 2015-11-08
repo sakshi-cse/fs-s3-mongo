e28536a9bd419451cd6f612bd060a895dd0fc39a
Brian Silvia <brian@brinkbit.com>
2015-11-08 14:52:22 -0500

    Stub out top level export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const mongoModule = require( './mongo.js' );
+const s3Module = require( './s3.js' );
+
+module.exports = {
+    fsSearch: mongoModule.search,
+    fsUpdate: mongoModule.update,
+    fsDelete: mongoModule.destroy,
+    readResource: s3Module.read,
+    writeResource: s3Module.write,
+    copyResource: s3Module.copy,
+    deleteResource: s3Module.destroy,
+};
