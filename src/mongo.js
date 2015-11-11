a41e1163b3a3f08631c2877e0463bf06e4ee826c
Brian Silvia <brian@brinkbit.com>
2015-11-11 01:14:52 -0500

    Fix not implemented return value

--- src/mongo.js
+++ src/mongo.js
@@ -1,21 +1,23 @@
 'use strict';
 
+const utils = require( './utils.js' );
+
 /* eslint no-unused-vars: 0 */
 
 module.exports.search = function search( toFind ) {
     // TODO: hit mongo and search
 
-    return Promise.reject({ code: 501, message: 'Not implemented.' });
+    return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
 module.exports.update = function update( toFind, toUpdate ) {
     // TODO: hit mongo and update
 
-    return Promise.reject({ code: 501, message: 'Not implemented.' });
+    return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
 module.exports.destroy = function destroy( toFind ) {
     // TODO: hit mongo and delete
 
-    return Promise.reject({ code: 501, message: 'Not implemented.' });
+    return Promise.reject( 'NOT_IMPLEMENTED' );
 };
