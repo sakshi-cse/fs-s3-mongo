f9b7c091cc2eac3e5b3bb109f7cad0d0c58bbff9
Brian Silvia <brian@brinkbit.com>
2015-11-11 02:09:55 -0500

    Update parameter names

--- src/mongo.js
+++ src/mongo.js
@@ -4,19 +4,21 @@ const utils = require( './utils.js' );
 
 /* eslint no-unused-vars: 0 */
 
-module.exports.search = function search( toFind ) {
+module.exports.search = function search( pathObj ) {
     // TODO: hit mongo and search
 
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.update = function update( toFind, toUpdate ) {
+module.exports.update = function update( pathObj, data ) {
     // TODO: hit mongo and update
+
+    //
 
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.destroy = function destroy( toFind ) {
+module.exports.destroy = function destroy( pathObj ) {
     // TODO: hit mongo and delete
 
     return Promise.reject( 'NOT_IMPLEMENTED' );
--- src/s3.js
+++ src/s3.js
@@ -2,25 +2,25 @@
 
 /* eslint no-unused-vars: 0 */
 
-module.exports.read = function read( toFind ) {
+module.exports.read = function read( path ) {
     // TODO: hit bucket and read
 
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.write = function write( toWrite ) {
+module.exports.write = function write( path, content ) {
     // TODO: hit bucket and write
 
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.copy = function copy( toFind, destination ) {
+module.exports.copy = function copy( path, destination ) {
     // TODO: hit bucket and copy
 
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.destroy = function destroy( toFind ) {
+module.exports.destroy = function destroy( path ) {
     // TODO: hit mongo and delete
 
     return Promise.reject( 'NOT_IMPLEMENTED' );
