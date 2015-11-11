3272eaa21d1e461e47b94e96db619cb6d4c29cf1
Brian Silvia <brian@brinkbit.com>
2015-11-11 01:16:34 -0500

    Fix not implemented return value

--- src/s3.js
+++ src/s3.js
@@ -5,23 +5,23 @@
 module.exports.read = function read( toFind ) {
     // TODO: hit bucket and read
 
-    return Promise.reject({ code: 501, message: 'Not implemented.' });
+    return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
 module.exports.write = function write( toWrite ) {
     // TODO: hit bucket and write
 
-    return Promise.reject({ code: 501, message: 'Not implemented.' });
+    return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
 module.exports.copy = function copy( toFind, destination ) {
     // TODO: hit bucket and copy
 
-    return Promise.reject({ code: 501, message: 'Not implemented.' });
+    return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
 module.exports.destroy = function destroy( toFind ) {
     // TODO: hit mongo and delete
 
-    return Promise.reject({ code: 501, message: 'Not implemented.' });
+    return Promise.reject( 'NOT_IMPLEMENTED' );
 };
