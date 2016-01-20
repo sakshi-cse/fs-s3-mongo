8c7369f77e8a93f6f383362adaf4a27fcf6314f3
Brian Silvia <brian@brinkbit.com>
2016-01-20 00:45:22 -0500

    Clean up the stubbed out s3.js

--- src/s3.js
+++ src/s3.js
@@ -2,26 +2,27 @@
 
 /* eslint no-unused-vars: 0 */
 
-module.exports.read = function read( path ) {
-    // TODO: hit bucket and read
-
+// Read content of s3 bucket by GUID
+module.exports.read = function read( GUID ) {
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.write = function write( path, content ) {
-    // TODO: hit bucket and write
-
+// Write GUID: content to s3 bucket
+module.exports.write = function write( GUID, content ) {
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.copy = function copy( path, destination ) {
-    // TODO: hit bucket and copy
-
+// Copy one GUID's content to another GUID
+module.exports.copy = function copy( fromGUID, toGUID ) {
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
 
-module.exports.destroy = function destroy( path ) {
-    // TODO: hit mongo and delete
+// Delete from s3 by GUID
+module.exports.destroy = function destroy( GUID ) {
+    return Promise.reject( 'NOT_IMPLEMENTED' );
+};
 
+// Grab content by GUID, then compress and return to the client
+module.exports.download = function download( GUID, compressionType ) {
     return Promise.reject( 'NOT_IMPLEMENTED' );
 };
