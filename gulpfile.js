33135c1182d8cc7021da250088bb6171e58559e8
Justin Livi <justin@brinkbit.com>
2016-03-17 17:58:26 -0400

    Update test config with coverage

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,26 @@
 
 const gulp = require( 'gulp' );
 const mocha = require( 'gulp-mocha' );
+const isparta = require( 'isparta' );
+const istanbul = require( 'gulp-istanbul' );
 
-gulp.task( 'test', () => {
-    return gulp.src([ 'src/**/*.js', 'test/**/*.js' ], { read: false })
+gulp.task( 'pre-test', () => {
+    return gulp.src( 'src/**/*.js' )
+    .pipe( istanbul({
+        instrumenter: isparta.Instrumenter,
+    }))
+    .pipe( istanbul.hookRequire());
+});
+
+gulp.task( 'test', ['pre-test'], () => {
+    return gulp.src([
+        'test/mongo.js',
+        'test/s3.js',
+        'test/index.js',
+    ])
     .pipe( mocha())
-    .once( 'end', () => process.exit());
+    .pipe( istanbul.writeReports())
+    .once( 'end', () => {
+        process.exit();
+    });
 });
