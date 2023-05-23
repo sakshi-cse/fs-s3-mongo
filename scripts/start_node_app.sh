78bd5bf9a8e3a1ec18c259a45de20b2534664924
Sakshi <74068861+sakshi-cse@users.noreply.github.com>
2023-05-23 10:49:22 +0530

    Update start_node_app.sh

--- scripts/start_node_app.sh
+++ scripts/start_node_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Go to the application directory
-cd /path/to/application
+cd /var/www/html
 
 # Start the Node.js application
 node app.js  # Replace "app.js" with the main file of your application
